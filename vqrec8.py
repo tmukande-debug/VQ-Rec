e7c15eb0c7b78d8aa941a01a90ef68dc427dc474
Tendai Mukande <75079602+tmukande-debug@users.noreply.github.com>
2023-03-20 13:58:25 +0000

    Update vqrec8.py

--- vqrec8.py
+++ vqrec8.py
@@ -10,6 +10,74 @@ from sinkhorn_transformer import SinkhornTransformer
 #soft-margin softmax (SM-softmax)
 #routing-transformer, reformer
 
+"""Implementation of Lehmer Code.
+Permutation - an array of integers. For the given interval, the array has to contains all numbers
+for the interval and each of the number has to appear only once.
+GitHub: https://github.com/mateuszchudyk/lehmer
+"""
+
+__author__ = "Mateusz Chudyk"
+__license__ = "MIT"
+
+def static_var(varname, value):
+    def decorate(func):
+        setattr(func, varname, value)
+        return func
+    return decorate
+
+@static_var("lut", [1])
+def factorial(n):
+    while n >= len(factorial.lut):
+        factorial.lut.append(factorial.lut[-1] * len(factorial.lut))
+    return factorial.lut[n]
+
+def encode(permutation):
+    """Return Lehmer Code of the given permutation.
+    """
+    def permutation_is_valid(permutation):
+        if not permutation:
+            return False
+
+        minimum = min(permutation)
+        maximum = max(permutation)
+
+        used = [0] * (maximum - minimum + 1)
+        for i in permutation:
+            used[i - minimum] += 1
+
+        if min(used) == 1 and max(used) == 1:
+            return True
+        else:
+            return False
+
+    def count_lesser(i, permutation):
+        return sum(it < permutation[i] for it in permutation[i + 1:])
+    
+    def parial_result(i, permutation):
+        return count_lesser(i, permutation) * factorial(len(permutation) - 1 - i)
+
+    if not permutation_is_valid(permutation):
+        return False
+    
+    return sum(parial_result(i, permutation) for i in range(0, len(permutation)))
+
+def decode(length, lehmer):
+    """Return permutation for the given Lehmer Code and permutation length. Result permutation contains
+    number from 0 to length-1.
+    """
+    result = [(lehmer % factorial(length - i)) // factorial(length - 1 - i) for i in range(length)]
+    used = [False] * length
+    for i in range(length):
+        counter = 0
+        for j in range(length):
+            if not used[j]:
+                counter += 1
+            if counter == result[i] + 1:
+                result[i] = j
+                used[j] = True
+                break
+    return result
+
 def log(t, eps = 1e-6):
     return torch.log(t + eps)
 
